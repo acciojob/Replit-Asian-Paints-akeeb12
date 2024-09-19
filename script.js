03932c998224881943e63f02aa109c12d32c4184
akeeb12 <112759560+akeeb12@users.noreply.github.com>
2024-09-19 08:31:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+document.getElementById('change_button').addEventListener('click', function () {
+	for(let i = 1; i <= 9; i ++){
+		document.getElementById(`grid-items${i}`).style.backgroundColor = 'transparent';
+	}
+	let blockId = document.getElementById('block_id').value;
+	let colorId = document.getElementById('color_id').value;
+	document.getElementById(`grid-item${blockId}`).style.backgroundColor = color;
+});
+
+document.getElementById('Reset').addEventListener('click', function() {
+		for(let i = 1; i <= 9; i ++){
+			document.getElementById(`grid-item${i}`).style.backgroundColor = 'transparent';
+		}
+	});
